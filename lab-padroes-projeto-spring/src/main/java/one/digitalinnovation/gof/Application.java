b5a518e609f09c29522a49c9e5dbbaed1fecfb1f
Aline Sanches <sanches.aline6@gmail.com>
2021-10-19 20:06:23 -0300

    Initial commit

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+* Projeto Spring Boot gerado via Spring Initializr
+* Os seguintes módulos foram selecionados:
+* - Spring Data JPA
+* - Spring Web
+* - H2 Database
+* - OpenFeign*/
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
